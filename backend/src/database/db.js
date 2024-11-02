3aec02fdd59232db817a06b468afe4c008b48e19
maath.j <mateua12392@gmail.com>
2024-11-02 18:33:30 -0300

    Correcao .env

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -1,10 +1,9 @@
 import mongoose, { connect } from "mongoose";
 
 const connectMongo = async () => {
-    await mongoose.connect(process.env.URL).then((res) => {
+    await mongoose.connect(process.env.MONGO_URL).then((res) => {
         console.log("Conectado")
     })
 }
 
 export default connectMongo
-
