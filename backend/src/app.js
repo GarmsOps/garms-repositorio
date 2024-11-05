eb9defc46c11ce62e3eb34707e7b7e0d4db2dc27
maath.j <mateua12392@gmail.com>
2024-11-05 09:10:13 -0300

    Correção Cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 3033;
 
 app.use(cors({
-    origin: 'https://garmsops.com.br',
+    origin: 'http://garmsops.com.br',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
