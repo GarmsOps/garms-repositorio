f7fae0e2f4af2ec9b5128f9296c066eac646874a
Mateus Sousa <150763278+MaathJason@users.noreply.github.com>
2024-12-03 10:01:21 -0300

    Correção logo .jsx

--- frontend/client/src/components/LogoEtec.jsx
+++ frontend/client/src/components/LogoEtec.jsx
@@ -2,7 +2,7 @@ import './LogoEtec.css'
 
 function LogoEtec() {
     return (
-        <img src="img/etec.png" alt="Logo Etec Embu" />
+        <img src="./img/etec.png" alt="Logo Etec Embu" />
     )
 }
 
